9df6f51b4eef914ca17e423efe99f1e10d8a6b64
Christopher Lynch <YOUR-EMAIL-ADDRESS>
2014-12-12 13:19:02 -0800

    I got the logic working

--- README.md
+++ README.md
@@ -4,40 +4,7 @@ The game board will appear on the screen. iife
 The person who initiates the game will be X, The second player will be O.
 
 
-
- X will click on his choice of any of the 9 tiles that I think will be div class="tile") and that 
-tile will be changed to an X.
-
-The game board will search for a winner
-
-It will be Os turn and he can then click on any tile other than the tile that
-X has already chosen. if O clicks on a tile that X has already chosen, nothing will happen.
-
-The game board will search for a winner if there is a winner that winner will be displayed as an alert
-
-X has his choce of 7 tiles, that have not been chosen when he clicks on one, it will change to an X
-
-The game board will search for a winner if there is a winner that winner will be displayed as an alert
-
-O has his choice of 6 tiles that have not been chosen. when he clicks on one, it will change to an O.
-
-The game board will search for a winner if there is a winner that winner will be displayed as an alert
-
-X has his choice of 5 tiles that have not been chosen. when he clicks on one, it will change to an X.
-
-The game board will search for a winner if there is a winner that winner will be displayed as an alert
-
-O has his choice of 4 tiles that have not been chosen. when he clicks on one, it will change to an O.
-
-The game board will search for a winner if there is a winner that winner will be displayed as an alert
-
-X has his choice of 3 tiles that have not been chosen. when he clicks on one, it will change to an X.
-
-The game board will search for a winner if there is a winner that winner will be displayed as an alert and the game will end, 
-
-O has his choice of 2 tiles that have not been chosen. when he clicks on one, it will change to an O.
-
-There is now only 1 tile left
+At the beginning of the page
 
 
 
--- js/tictactoeController.js
+++ js/tictactoeController.js
@@ -1,3 +1,4 @@
+// function(){
 angular
 	.module('tictactoeApp')
 	.controller('TictactoeController',TictactoeController);
@@ -6,39 +7,71 @@ angular
 function TictactoeController(){
 var self = this;
 
-
+	//clickBox changes the box 
 	self.clickBox= clickBox;
+	//this is what created the board
 	self.Board = new Array ( 9 );
+	//turncounter
 	self.turncounter= 1;
+	self.winner = "";
 
 	function clickBox($index){
 		//this is the condition
-		if (self.Board[$index]===undefined){
-			if (self.turncounter===1){
+		if (self.Board[$index]==undefined){
+			if (self.turncounter==1){
 				self.Board[$index]="X";
-				self.turncounter=2;
+				self.turncounter=2;	
 			}
 			else{ 
 				self.Board[$index]="O";
 				self.turncounter=1;
 			}
-
 		}
+		
+		return winner();	
+
+	}
+	//this will determine the winner 
+	function winner(){
+		if ((self.Board[0]==="X" && self.Board[1]==="X" && self.Board[2]==="X")||
+			(self.Board[3]==="X" && self.Board[4]==="X" && self.Board[5]==="X")||
+			(self.Board[6]==="X" && self.Board[7]==="X" && self.Board[8]==="X")||
+			(self.Board[0]==="X" && self.Board[3]==="X" && self.Board[6]==="X")||
+			(self.Board[1]==="X" && self.Board[4]==="X" && self.Board[7]==="X")||
+			(self.Board[2]==="X" && self.Board[5]==="X" && self.Board[8]==="X")||
+			(self.Board[0]==="X" && self.Board[4]==="X" && self.Board[8]==="X")||
+			(self.Board[2]==="X" && self.Board[4]==="X" && self.Board[6]==="X")){
+				console.log("X is the winner");
+			    return "X is the winner";
+				}
+			// console.log($index);
+		if ((self.Board[0]==="O" && self.Board[1]==="O" && self.Board[2]==="O")||
+			(self.Board[3]==="O" && self.Board[4]==="O" && self.Board[5]==="O")||
+			(self.Board[6]==="O" && self.Board[7]==="O" && self.Board[8]==="O")||
+			(self.Board[0]==="O" && self.Board[3]==="O" && self.Board[6]==="O")||
+			(self.Board[1]==="O" && self.Board[4]==="O" && self.Board[7]==="O")||
+			(self.Board[2]==="O" && self.Board[5]==="O" && self.Board[8]==="O")||
+			(self.Board[0]==="O" && self.Board[4]==="O" && self.Board[8]==="O")||
+			(self.Board[2]==="O" && self.Board[4]==="O" && self.Board[6]==="O")){
+				console.log("O is the winner");
+				return "O is the winner";
+
+				}
+
+
 	}
 
 
 
-};
 
 
-//will need to use ng-bind somewhere in this application
+
+}
+// }());
+
+
 
 //need a function to look for the winner
-//potentially set this up by assigning the boxes marked x with a 1 and the boxes marked O with a 5
-
-//need a function to create a player object
-
-//need a function to toggle between players 
 
 //need a function for clear board
 //need a function fpr start game
