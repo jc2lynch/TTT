ab62fd13dec2f215bbcf9619c57adc8b9cf7b2be
Christopher Lynch <YOUR-EMAIL-ADDRESS>
2014-12-12 17:25:52 -0800

    I finally got the new game function to work

---
+++ js/full.js
@@ -0,0 +1,6 @@
+// Array.prototype.fullArray = function() {
+
+// 	for(var i= 0; 1<=9;i++){
+// 		console.log("index[" + i + "] " + this[i]);
+// 	}
+// }
--- js/tictactoeController.js
+++ js/tictactoeController.js
@@ -14,6 +14,8 @@ var self = this;
 	//turncounter
 	self.turncounter= 1;
 	self.winner = "";
+	// console.log(self.Board.fullArray())
+	self.newGame= newGame;
 
 	function clickBox($index){
 		//this is the condition
@@ -28,7 +30,7 @@ var self = this;
 			}
 		}
 		
-		return winner();	
+		winner();	
 
 	}
 	//this will determine the winner 
@@ -41,11 +43,11 @@ var self = this;
 			(self.Board[2]==="X" && self.Board[5]==="X" && self.Board[8]==="X")||
 			(self.Board[0]==="X" && self.Board[4]==="X" && self.Board[8]==="X")||
 			(self.Board[2]==="X" && self.Board[4]==="X" && self.Board[6]==="X")){
-				console.log("X is the winner");
-			    return "X is the winner";
+				alert("X is the winner");
+			    // return "X is the winner";
 				}
 			// console.log($index);
-		if ((self.Board[0]==="O" && self.Board[1]==="O" && self.Board[2]==="O")||
+		else if ((self.Board[0]==="O" && self.Board[1]==="O" && self.Board[2]==="O")||
 			(self.Board[3]==="O" && self.Board[4]==="O" && self.Board[5]==="O")||
 			(self.Board[6]==="O" && self.Board[7]==="O" && self.Board[8]==="O")||
 			(self.Board[0]==="O" && self.Board[3]==="O" && self.Board[6]==="O")||
@@ -53,13 +55,23 @@ var self = this;
 			(self.Board[2]==="O" && self.Board[5]==="O" && self.Board[8]==="O")||
 			(self.Board[0]==="O" && self.Board[4]==="O" && self.Board[8]==="O")||
 			(self.Board[2]==="O" && self.Board[4]==="O" && self.Board[6]==="O")){
-				console.log("O is the winner");
-				return "O is the winner";
+				alert("O is the winner");
+				// return "O is the winner";
 
 				}
+			// else (self.board[0]!= null && self.board[1]!= null && self.board[0]!= null && self.board[2]!= null && self.board[3]!= null && self.board[4]!= null &&
+			// self.board[5]!= null && self.board[6]!= null && self.board[7]!= null && self.board[8])
+			// 	console.log("cat's game")
+
+
 
 
 	}
+function newGame() {
+   self.Board = new Array (9);
+
+}
+
 
 
 
